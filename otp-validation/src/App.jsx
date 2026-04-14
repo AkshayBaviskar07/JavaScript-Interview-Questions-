cffaf6c549519fdd12a139a41ae1aabe186cfffa
akshay <baviskarakshay0702@gmail.com>
2026-04-14 18:58:38 +0530

    validating otp input

--- otp-validation/src/App.jsx
+++ otp-validation/src/App.jsx
@@ -5,11 +5,12 @@ const OTP_DIGIT_COUNT = 5;
 
 const App = () => {
   const [inputArr, setInputArr] = useState(new Array(OTP_DIGIT_COUNT).fill(""));
+  const [generateOtp, setGenerateOtp] = useState(0);
   const ref = useRef([]);
 
   useEffect(() => {
     ref.current[0]?.focus();
-  }, []);
+  }, [generateOtp]);
 
   const handleOnChange = (value, index) => {
     if(isNaN(value)) return;
@@ -18,20 +19,40 @@ const App = () => {
     const newValue = value.trim();
     newArr[index] = newValue.slice(-1);
     setInputArr(newArr);
-    newValue && ref.current[index + 1].focus();
+    newValue && ref.current[index + 1]?.focus();
   }
 
   function handleOnKeyDown(e, index) {
-    if (!e.target.value && e.key === "Backspace") {
+    if (!e.target.value && e.key === "Backspace" && index > 0) {
       ref.current[index - 1].focus();
     }
   }
 
+  // function to generate random number string of 5 digits
+  const handleGenerateOtp = () => {
+    let genOtp = Math.floor(Math.random() * 90000 + 10000).toString() ;
+    console.log(`OTP: ${genOtp}`);
+    setGenerateOtp(genOtp);
+    return generateOtp;
+  };
+
+  const verifyOtp = () => {
+    const enteredOtp = inputArr.join("");
+    if (enteredOtp === generateOtp) {
+      console.log("OTP Verified ✅");
+    } else {
+      console.log("Invalid OTP ❌");
+    }
+  };
+
   return (
-      <div>
-        <h1>Validate Otp</h1>
+      <div className={"container"}>
+        <header>
+          <h1>Validate Otp</h1>
+        </header>
+        <main>
         {
-          inputArr.map((input, index) => {
+            inputArr.map((input, index) => {
             return <input key={index}
                           className={'otp-input'}
                           value={inputArr[index]}
@@ -41,6 +62,14 @@ const App = () => {
             />
           })
         }
+
+        </main>
+        <main>
+          <button className="btn" onClick={handleGenerateOtp}>
+            Generate Otp
+          </button>
+          <button className="btn" onClick={verifyOtp}>Submit</button>
+        </main>
       </div>
   )
 }
